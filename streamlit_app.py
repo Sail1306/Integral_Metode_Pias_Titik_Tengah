502ec35ad2e405f92046acd34c84b62c3cd5a79a
Sail1306 <sailahidayah06@gmail.com>
2025-12-12 20:25:24 +0530

    Begin.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Integral Solution (Pias Titik Tengah)",
+    page_icon="📈",
+    layout="centered",
+    initial_sidebar_state="expanded"
 )
